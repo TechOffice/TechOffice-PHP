38e47e4e312772b9f170619944a173a20c1c2497
imben1109 <Ben_c@ITD-PC2220.mpfahk.org>
2017-02-13 11:59:53 +0800

    update

---
+++ 010-Core/040-Function/Network/HeaderExample/index.php
@@ -0,0 +1,18 @@
+<?php
+/*
+	Use PHP Header function to set Access-Control-Allow-Origin: *
+*/
+	ob_start();
+	header('Access-Control-Allow-Origin: *');
+	ob_end_flush();
+?>
+
+<html>
+	<head>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+	</head>
+	
+	<body>
+	
+	</body>
+</html>
--- 030-Tools/010-PackageManager/010-Composer/ComposerHelloWorldExample/Program.php
+++ 030-Tools/010-PackageManager/010-Composer/ComposerHelloWorldExample/Program.php
@@ -1,4 +1,8 @@
 <?php
+require_once __DIR__ . '/vendor/autoload.php';
+$log = new Monolog\Logger('name');
+$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+$log->addWarning('Foo');
 
 
 ?>
